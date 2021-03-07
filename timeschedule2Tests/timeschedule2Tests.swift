82bc3dd902805af8000b76999d4456e4516d505c
Masahiro Tonogai <tonomasa36@gmail.com>
2021-03-07 14:58:59 +0900

    Initial Commit

---
+++ timeschedule2Tests/timeschedule2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  timeschedule2Tests.swift
+//  timeschedule2Tests
+//
+//  Created by masahiro tono on 2021/03/07.
+//
+
+import XCTest
+@testable import timeschedule2
+
+class timeschedule2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
